ca0de659a6ecd330c781374b82bb97fb035bc92c
Nayani Tharushika <118815958+untabepala@users.noreply.github.com>
2024-07-13 07:30:37 +0530

    Add files via upload

---
+++ face1.py
@@ -0,0 +1,19 @@
+import cv2
+
+cap=cv2.VideoCapture(0)
+
+cascade=cv2.CascadeClassifier("open/haarcascade_frontalface_default.xml")
+
+while True:
+    success,frame=cap.read()
+    gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    faces=cascade.detectMultiScale(gray,1.3,5)
+
+    for (x,y,w,h) in faces:
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+
+        cv2.imshow("Webcam",frame)
+        if cv2.waitKey(1) & 0xFF ==ord("s"):
+            break
+
+cap.release()
